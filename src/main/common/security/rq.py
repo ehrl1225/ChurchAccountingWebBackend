87418ee2af7a0235055b889998477af8c67e810d
ehrl1225 <wjdwntls1225@naver.com>
2025-12-20 15:48:13 +0900

    member role 체크 함수 구현

--- src/main/common/security/rq.py
+++ src/main/common/security/rq.py
@@ -1,15 +1,19 @@
 from dependency_injector.wiring import inject, Provide
-from fastapi import HTTPException, Request, Response, Depends
+from fastapi import HTTPException, Request, Response, Depends, status
 from jose import JWTError
 from sqlalchemy.orm import Session
+from typing import Optional
 
-from domain.member.repository import RefreshTokenRepository
+from common.database.member_role import get_member_roles
+from domain.member.repository import RefreshTokenRepository, MemberRepository
+from domain.organization.joined_organization.repository import JoinedOrganizationRepository
+from domain.organization.organization.repository import OrganizationRepository
+from domain.organization.joined_organization.entity import JoinedOrganization
 from .jwt_util import decode_token, dict_to_member, set_token
 from domain.member.entity import RefreshToken
 from .member_DTO import MemberDTO
-from typing import Optional
-
 from common.dependency_injector import Container
+
 
 
 def get_current_user(token: str) -> Optional[MemberDTO]:
@@ -50,3 +54,26 @@ async def get_current_user_from_cookie(
         return dict_to_member(decoded_refresh_token)
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+@inject
+async def check_member_role(
+        db:Session,
+        member_id: int,
+        organization_id: int,
+        member_role_mask: int,
+        member_repository: MemberRepository = Depends(Provide[Container.member_repository]),
+        organization_repository: OrganizationRepository = Depends(Provide[Container.organization_repository]),
+        joined_organization_repository: JoinedOrganizationRepository = Depends(Provide[Container.joined_organization_repository])
+):
+    member_roles = get_member_roles(member_role_mask)
+    organization = await organization_repository.find_by_id(db, organization_id)
+    if not organization:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Organization not found")
+    member = await member_repository.find_by_id(db, member_id)
+    if not member:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Member not found")
+    joined_organization:JoinedOrganization = await joined_organization_repository.find_by_member_and_organization(db, member, organization)
+    if not joined_organization:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Member didn't joined organization")
+    if joined_organization.member_role not in member_roles:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Member role not exist")
