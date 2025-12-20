0287a31a8f1588bc598a0835c3bd42fc0eaa7af9
ehrl1225 <wjdwntls1225@naver.com>
2025-12-20 21:43:14 +0900

    organization delete 추가 (#28)

--- src/main/domain/organization/organization/controller/organization_controller.py
+++ src/main/domain/organization/organization/controller/organization_controller.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends, Request, Response, status
 from sqlalchemy.orm import Session
 from dependency_injector.wiring import inject, Provide
 
+from common.database.member_role import OWNER_ONLY_MASK
 from common.dependency_injector import Container
 from common.database import get_db
-from common.security.rq import get_current_user_from_cookie
+from common.security.rq import get_current_user_from_cookie, check_member_role
 from domain.organization.organization.service import OrganizationService
 from domain.organization.organization.dto import OrganizationCreateDto
 
@@ -19,6 +20,28 @@ async def create_organization(
         db:Session = Depends(get_db),
         organization_service:OrganizationService = Depends(Provide[Container.organization_service])
 ):
-    member = await get_current_user_from_cookie(request, response, db)
-    await organization_service.create(db, member, organization)
+    try:
+        member = await get_current_user_from_cookie(request, response, db)
+        await organization_service.create(db, member, organization)
+        db.commit()
+    except Exception as err:
+        db.rollback()
+        raise err
 
+@router.delete("/{organization_id}", status_code=status.HTTP_204_NO_CONTENT)
+@inject
+async def delete_organization(
+        request:Request,
+        response:Response,
+        organization_id: int,
+        db:Session = Depends(get_db),
+        organization_service:OrganizationService = Depends(Provide[Container.organization_service]),
+):
+    try:
+        me_dto = await get_current_user_from_cookie(request, response, db)
+        await check_member_role(db, me_dto.id, organization_id, OWNER_ONLY_MASK)
+        await organization_service.delete(db, organization_id)
+        db.commit()
+    except Exception as err:
+        db.rollback()
+        raise err
